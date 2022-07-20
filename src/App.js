e1e03d39f0929fb4e5ed6f5a0ebb0c6bef151bc5
Pranjal Srivastava <prns366@gmail.com>
2022-07-20 11:04:30 +0530

    docs: added Result Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { useLoader } from "contexts";
 import { Route, Routes } from "react-router-dom";
-import { Navbar } from "./components";
-import { Home } from "./pages";
+import { Loader, Navbar } from "./components";
+import { Home, QuizQuestion, Result, Rules } from "./pages";
 
 function App() {
+  const { showLoader } = useLoader();
   return (
     <div>
+      {showLoader && <Loader />}
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/rules" element={<Rules />} />
+        <Route path="/quiz" element={<QuizQuestion />} />
+        <Route path="/result" element={<Result />} />
       </Routes>
     </div>
   );
