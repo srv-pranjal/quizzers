719c5f0d6fee391b8ac28ce52c66d1b6aa809344
srv-pranjal <prns366@gmail.com>
2022-08-04 23:54:58 +0530

    docs: added private routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import { useLoader } from "contexts";
 import { Route, Routes } from "react-router-dom";
 import { Loader, Navbar } from "./components";
-import { Home, QuizQuestion, Result, Rules } from "./pages";
-
+import {
+  Home,
+  Login,
+  PrivateRoutes,
+  Profile,
+  QuizQuestion,
+  Result,
+  Rules,
+  Signup,
+} from "./pages";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   const { showLoader } = useLoader();
+
   return (
     <div>
       {showLoader && <Loader />}
+      <ToastContainer autoClose="1500" closeOnClick="true" />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/rules" element={<Rules />} />
-        <Route path="/quiz" element={<QuizQuestion />} />
-        <Route path="/result" element={<Result />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route element={<PrivateRoutes />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/rules" element={<Rules />} />
+          <Route path="/quiz" element={<QuizQuestion />} />
+          <Route path="/result" element={<Result />} />
+        </Route>
       </Routes>
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,7 @@ export { Home } from "./Home/Home";
 export { Rules } from "./Rules/Rules";
 export { QuizQuestion } from "./QuizQuestion/QuizQuestion";
 export { Result } from "./Result/Result";
+export { Login } from "./Auth/Login";
+export { Signup } from "./Auth/Signup";
+export { Profile } from "./Profile/Profile";
+export { PrivateRoutes } from "./PrivateRoutes/PrivateRoutes";
